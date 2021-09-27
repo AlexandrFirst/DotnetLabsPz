f04054cf68215cb36c769f3db68a9ad4c59f7877
Olexandr Lohvinov <ato31ato@gmail.com>
2021-09-27 19:29:37 +0300

    Project restructure

---
+++ Test_PZ1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_PZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
